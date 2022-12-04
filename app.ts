366e289986828fd95efd81ca95c94da94d47b3b5
Slava Che <xblindsx@gmail.com>
2022-12-04 15:31:44 +0200

    19 Tuples

--- app.ts
+++ app.ts
@@ -1,12 +1,18 @@
-const person = {
-    name: "Vasya",
-    age: 20,
-    hobbies: ["sports", "warhammer"]
-}
+const person: {
+    name: string;
+    age: number;
+    hobbies: string[],
+    role: [number, string] //tuple
+} = {
+  name: "Vasya",
+  age: 20,
+  hobbies: ["sports", "warhammer"],
+  role: [2, "author"],
+};
 
 for (const hobby of person.hobbies) {
-    //Type inference will help IDE to understand what type of element in array will be and autosuggest correct properties and methods;
-    console.log(hobby.toUpperCase());
+  //Type inference will help IDE to understand what type of element in array will be and autosuggest correct properties and methods;
+  console.log(hobby.toUpperCase());
 }
 
 console.log(person);
