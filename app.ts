f623407041c2078bbe23eb1a251d06aa56542946
inteligenzia <xblindsx@gmail.com>
2023-01-10 13:27:30 +0200

    video 28

--- app.ts
+++ app.ts
@@ -12,8 +12,19 @@ const f = (n: number): void => {
     console.log("Add: " + n);
 }
 
-type combineFunction = (a: number, b: number): number => number
+type combineFunction = (a: number, b: number) => number
 
 let combineValues: combineFunction; 
 
 combineValues = add; // will work; 
+
+function sendRequest(data: string, cb: (response: any) => void) {
+    // ... sending a request with "data"
+    console.log(cb);
+    return cb({data: data});
+  }
+
+  sendRequest('Send this!', (response) => { 
+    console.log(response);
+    return true;
+   });
